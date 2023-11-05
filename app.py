94cd0c9eb179efc5d12243f5fe76f3b2bf510944
sapana-k <sapanakhedkar957@gmail.com>
2023-11-06 00:08:01 +0530

    converts pdf to txt

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import PyPDF2
+
+def pdf_to_text(uploaded_pdf):
+    text = ''
+    pdf_reader = PyPDF2.PdfReader(uploaded_pdf)
+    for page in range( len(pdf_reader.pages)):
+        text += pdf_reader.pages[page].extract_text()
+    return text
+
 
 st.set_page_config(page_title="BookQuester", page_icon="📖", layout="wide")
 st.header("📖BookQuester")
@@ -10,5 +19,9 @@ uploaded_file = st.file_uploader(
     type=["pdf", "docx", "txt"],
     help="Scanned documents are not supported yet!",
 )
-
+if uploaded_file:
+    text = pdf_to_text(uploaded_file)
+    with open("extracted_text.txt", "w", encoding="utf-8") as file:
+        file.write(text)
+    
 st.write(uploaded_file)
